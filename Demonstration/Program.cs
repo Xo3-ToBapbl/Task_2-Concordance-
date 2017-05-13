2c1502a996c85cb60697badfe9f8119a11e182f0
Jhon <jhonstupak@gmail.com>
2017-05-13 12:01:47 +0300

    Start

--- Demonstration/Program.cs
+++ Demonstration/Program.cs
@@ -4,9 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Demonstration
+namespace Concordance
 {
-    class Program
+    class Demonstration
     {
         static void Main(string[] args)
         {
